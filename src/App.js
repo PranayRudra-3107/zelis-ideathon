6cc3c9099871f99356965f23d39dda565383be4a
5aiteja <saiteja.ankam@zelis.com>
2023-11-09 11:15:44 +0000

    final

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import React, { Component } from 'react';
 
-export default function BasicTextFields() {
-  return (
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      
-    </Box>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
 }
+
+export default App;
