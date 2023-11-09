aababb34c9cc3a4ecaa96459a2b871fc5afcfd3c
5aiteja <saiteja.ankam@zelis.com>
2023-11-09 11:07:15 +0000

    last

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ export default function BasicTextFields() {
       autoComplete="off"
     >
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-      <TextField id="standard-basic" label="Standard" variant="standard" />
+      
     </Box>
   );
 }
