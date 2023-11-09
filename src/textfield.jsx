5d2f92ace1ac9d98f6d0aab32187685fd99711d1
Pranay Rudra <rudra.shivapranay@gmail.com>
2023-11-09 16:26:52 +0530

    commit

--- src/textfield.jsx
+++ src/textfield.jsx
@@ -15,6 +15,9 @@ export default function BasicTextFields() {
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
       <TextField id="filled-basic" label="Filled" variant="filled" />
       <TextField id="standard-basic" label="Standard" variant="standard" />
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
     </Box>
   );
 }
