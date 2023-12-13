6906114be50287a61366b2ed8054e779399f23a5
5aiteja <saiteja.ankam@zelis.com>
2023-12-13 16:02:28 +0530

    update env

--- node-postgres/backend/index.js
+++ node-postgres/backend/index.js
@@ -9,7 +9,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PG_PORT,
+  port: process.env.DB_PGPORT,
 });
 
 module.exports = pool;
